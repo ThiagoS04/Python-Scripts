ceed5ff76f5f33e659d57b18270904a6fcac4160
ThiagoS04 <schuck.thiago@student.ccm.edu>
2024-02-15 14:03:27 -0500

    Class Methods and GUI

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -5,21 +5,85 @@
 def main():
     
     # Import modules
-    import random
+    global BattleshipClasses
+    import BattleshipClasses
 
-    # Create the board
+    # Declare fields
+    playerList = []
 
     # Ask if user wants to play against AI or another player
+    playAI = True if input("Do you want to play against AI? (y/n): ") == "y" else False
+    numPlayers = 1 if playAI else 2     # Save the number of players
 
-    # Create the player class
+    # Create the player class objects
+    for i in range(numPlayers):     # Loop through the number of players
+        playerList.append(create_player(numPlayers))       # Call the function to create the player class
+
+    if numPlayers == 1:      # If the user is playing against AI
+        # Ask for difficulty
+        difficulty = int(input("What difficulty do you want to play on?\n"
+                               "1. Easy\n"
+                               "2. Medium\n"
+                               "3. Hard\n"))
+        # Create the AI class object
+        AI = BattleshipClasses.AI(difficulty)      # Call the function to create the AI class
+        playerList.append(AI)      # Add the AI to the player list
+
+    # Create the board
+    create_board(playerList)      # Call the function to create the board GUI
 
 
-
-# Function to create player class
-def create_player():
-    
+# Function to create player class with number of players as parameter
+def create_player(numPlayers):
+        
     # Ask for player name
-
-    # Ask where the user wants to place the ships
+    name = input("What is your name?")
 
     # Create the player object
+    player = BattleshipClasses.Player(name)
+
+    # Return player reference
+    return player
+
+
+# Function to create the board GUI
+def create_board(playerList):
+
+    # Import modules
+    import PySimpleGUI as sg
+
+    # Declare fields
+    boardSize = 10      # Board size
+    layout = []         # Layout of GUI list
+    length = 2          # Length of the buttons
+    width = 1           # Width of the buttons
+
+    # Create the board layout
+    leftBoard = [(sg.Button(" ", size=(length, width), pad=(0,0), key=(i,j)) for j in range(boardSize)) for i in range(boardSize)]
+    leftBoardName = [(sg.Text(playerList[0].name, size = (10,1), pad = (0,0), justification="center"))]
+
+    rightBoard = [(sg.Button(" ", size=(length, width), pad=(0,0), key=(i,j)) for j in range(boardSize)) for i in range(boardSize)]
+    rightBoardName = [(sg.Text(playerList[1].name, size = (10,1), pad = (0,0), justification = "center"))]
+
+    layout = [
+        [sg.Column([leftBoardName]), sg.VerticalSeparator(pad=(boardSize * 13.6 + 100, 0)), sg.Column([rightBoardName])],
+        [sg.Column(leftBoard), sg.VerticalSeparator(pad=(100, 0)),  sg.Column(rightBoard)]
+        ]
+
+    # Create board
+    window = sg.Window("Battleship", layout)
+
+    # Event loop
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if isinstance(event, tuple) and len(event) == 2:
+            print(event)
+            print(event[1])
+
+# Start the game
+if __name__ == "__main__":
+    main()
--- Battleship/BattleshipClasses.py
+++ Battleship/BattleshipClasses.py
@@ -1,9 +1,55 @@
 # Thiago Schcuk 23 January 2024
 # This script is the class file for the battleship game. It contains the classes for the AI and the player.
 
+# Player class
 class Player:
 
-    # Player object constructor with
-    def __init__(self, name, score):
+    # Declare class attributes
+    ships = {}    # {Ship:position}
+
+    # Player object constructor with name and ships position
+    def __init__(self, name):
         self.name = name
-        self.score = score
+
+    # Function to choose ship positions
+    def choose_ships(self):
+        pass
+
+    # Function to choose a position to attack
+    def choose_position(self):
+        pass
+
+# AI class
+class AI(Player):
+
+    # Declare class attributes
+    name = "AI"
+    ships = {}    # {Ship:position}
+
+    # AI object constructor with difficulty and ships position
+    def __init__(self, difficulty):
+        self.difficulty = difficulty    # Save the difficulty
+
+        super().__init__(self.name)   # Call the parent class constructor
+
+    # Function to choose ship positions based on diffiuclty
+    def choose_ships(self):
+        
+        # Declare ships
+
+        # Get difficulty
+        if self.difficulty == 1:
+            pass
+        
+        elif self.difficulty == 2:
+            pass
+
+        elif self.difficulty == 3:
+            pass
+
+        else:
+            print("Invalid difficulty")
+
+    # Function to choose a random position to attack
+    def choose_position(self):
+        pass
